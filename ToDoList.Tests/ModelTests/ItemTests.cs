d8fc9c21e2875ef0c3a89719fd63d0be888e5468
Nathan Conn <nconn34@gmail.com>
2022-02-23 09:39:04 -0800

    First commit for C# practice

---
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -0,0 +1,91 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using ToDoList.Models;
+using System;
+
+namespace ToDoList.Tests
+{
+  [TestClass]
+  public class ItemTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string updatedDescription = "Do the dishes";
+      newItem.Description = updatedDescription;
+      string result = newItem.Description;
+      Assert.AreEqual(updatedDescription, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_ItemList()
+    {
+      List<Item> newList = new List<Item> { };
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2 };
+      List<Item> result = Item.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+     [TestMethod]
+  public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+  {
+    //Arrange
+    string description = "Walk the dog.";
+    Item newItem = new Item(description);
+
+    //Act
+    int result = newItem.Id;
+    //Assert
+    Assert.AreEqual(1, result);
+  }
+   [TestMethod]
+  public void Find_ReturnsCorrectItem_Item()
+  {
+    //Arrange
+    string description01 = "Walk the dog";
+    string description02 = "Wash the dishes";
+    Item newItem1 = new Item(description01);
+    Item newItem2 = new Item(description02);
+
+    //Act
+    Item result = Item.Find(2);
+
+    //Assert
+    Assert.AreEqual(newItem2, result);
+  }
+  }
+}
